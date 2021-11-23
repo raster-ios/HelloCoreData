9db44080be1de1d331c8c8b7f300e35ea371f6b6
Gennady Raster <raster.iosdev@gmail.com>
2021-11-23 22:57:41 +0300

    Initial Commit

---
+++ HelloCoreData/HelloCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoreDataApp.swift
+//  HelloCoreData
+//
+//  Created by Gena Raster on 23.11.21.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
